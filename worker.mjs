f9443e7334e9a907cdb72a98459feb8739d3f796
Nicky Thorne <nthorne@cloudbees.com>
2024-06-05 12:50:43 +0100

    add dist

--- worker.mjs
+++ worker.mjs
@@ -3,4 +3,3 @@ import WorkerContext from "./dist/worker/index.js"
 export const expose = WorkerContext.expose
 export const registerSerializer = WorkerContext.registerSerializer
 export const Transfer = WorkerContext.Transfer
-export const isWorkerRuntime = WorkerContext.isWorkerRuntime
